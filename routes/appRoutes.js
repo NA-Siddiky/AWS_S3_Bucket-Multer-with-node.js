4ee1a4d74430c72d2853caffcfb4c1d9f8b4d57a
NA-Siddiky <siddiky.academic@gmail.com>
2022-08-08 02:43:25 +0600

    file upload successfull

--- routes/appRoutes.js
+++ routes/appRoutes.js
@@ -2,6 +2,20 @@ const express = require("express");
 const route = express.Router();
 const appController = require("../controllers/fileUpload");
 
-route.post("/upload", appController.handleFileUpload);
+const multer = require('multer')
+const upload = multer({ dest: 'uploads/' })
+
+route.post("/upload", upload.single("File"), appController.handleFileUpload);
+
+route.post('/upload', upload.single('image'), async (req, res) => {
+    const file = req.file
+    console.log(file)
+
+    // apply filter
+    // resize 
+
+    const result = await appController.handleFileUpload
+})
+
 
 module.exports = route;
--- server.js
+++ server.js
@@ -1,8 +1,37 @@
-const app = require("./index");
-const http = require("http");
+// const app = require("./index");
+// const http = require("http");
 
-const server = http.createServer(app);
+// const server = http.createServer(app);
 
-server.listen(3001, () => {
-    console.log("server is running at port 3001");
-});
+// app.use((error, req, res, next) => {
+//     const message = `error --> "${error}"`
+//     console.log(message);
+//     return res.status(500).send(message);
+// })
+
+// server.listen(3001, () => {
+//     console.log("server is running at port 3001");
+// });
+
+const express = require('express')
+const multer = require('multer');
+
+const UPLOADS_FOLDER = "./uploads"
+
+const upload = multer({
+    dest: UPLOADS_FOLDER
+})
+
+const app = express()
+
+app.get("/", upload.single(), (req, res) => {
+    res.send("Hello");
+})
+
+app.post("/upload", upload.single("avatar"), (req, res) => {
+    res.send("Hello");
+})
+
+app.listen(8080, () => {
+    console.log("listening on port 8080")
+})
