319c970162049bc00acfa808ebb3a7e3151acc4a
NA-Siddiky <siddiky.academic@gmail.com>
2022-08-08 13:41:05 +0600

    multiple file upload done

--- routes/appRoutes.js
+++ routes/appRoutes.js
@@ -9,25 +9,42 @@ const UPLOADS_FOLDER = "./uploads"
 const upload = multer({
     dest: UPLOADS_FOLDER,
     limits: {
-        fileSize: 5000000 //1 MB
+        fileSize: 10000000 //1 MB
     },
     fileFilter: (req, file, cb) => {
         console.log(file)
-        if (
-            file.mimetype === "image/png" ||
-            file.mimetype === "image/jpg" ||
-            file.mimetype === "image/jpeg"
-        ) {
-            cb(null, true);
-        } else {
-            cb(new Error(`file formate is not supported >> ${Error}`));
+        if (file.fieldname === 'photo') {
+            if (
+                file.mimetype === "image/png" ||
+                file.mimetype === "image/jpg" ||
+                file.mimetype === "image/jpeg"
+            ) {
+                cb(null, true);
+            } else {
+                cb(new Error(`file formate is not supported >> ${Error}`));
+            }
+        }
+        else if (file.fieldname === 'doc') {
+            if (
+                file.mimetype === "application/pdf"
+            ) {
+                cb(null, true);
+            } else {
+                cb(new Error(`file formate is not supported >> ${Error}`));
+            }
+        }
+        else {
+            cb(new Error(`There was an unknown error >> ${Error}`));
         }
     },
 
 })
 
 
-route.post("/upload", upload.single("photo"), appController.handleFileUpload);
+route.post("/upload", upload.fields([
+    { name: "photo", maxCount: 1 },
+    { name: "doc", maxCount: 1 },
+]), appController.handleFileUpload);
 
 
 module.exports = route;
